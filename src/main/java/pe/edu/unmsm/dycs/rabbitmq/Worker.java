b060b6bf210a061895b6508cb5e1c42f4c772fa6
frank Jonislla <frank.che88@gmail.com>
2018-10-21 08:05:59 -0500

    procesar mensajes

---
+++ src/main/java/pe/edu/unmsm/dycs/rabbitmq/Worker.java
@@ -0,0 +1,69 @@
+package pe.edu.unmsm.dycs.rabbitmq;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class Worker {
+
+
+	public static void main(String[] argv) throws Exception {
+		
+		//"amqp://bzizimsp:LKVNczGhhTNTsk-CxeLgdE7MAFt_8Nbq@chimpanzee.rmq.cloudamqp.com/bzizimsp"
+		String mqUrl = System.getenv().get("AMQ_URL");
+		String mqQueue = System.getenv().get("AMQ_QUEUE");
+
+		
+		System.out.println("Worker.main()" + mqUrl);
+		
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		
+		factory.setUri(mqUrl);
+		
+		final Connection connection = factory.newConnection();
+		
+		final Channel channel = connection.createChannel();
+
+		channel.queueDeclare(mqQueue, true, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		channel.basicQos(1);
+
+		final Consumer consumer = new DefaultConsumer(channel) {
+
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+					byte[] body) throws IOException {
+				String message = new String(body, "UTF-8");
+
+				System.out.println(" [x] Received '" + message + "'");
+				try {
+					doWork(message);
+				} finally {
+					System.out.println(" [x] Done");
+					channel.basicAck(envelope.getDeliveryTag(), false);
+				}
+			}
+		};
+		channel.basicConsume(mqQueue, false, consumer);
+	}
+
+	private static void doWork(String task) {
+		for (char ch : task.toCharArray()) {
+			if (ch == '.') {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException _ignored) {
+					Thread.currentThread().interrupt();
+				}
+			}
+		}
+	}
+
+}
